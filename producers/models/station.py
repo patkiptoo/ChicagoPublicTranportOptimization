e4b2df778520ca56a8183fb7ee1621cec93e49ba
patkiptoo <patkiptoo@users.noreply.github.com>
2021-02-22 19:50:58 -0500

    fix: a bug fix
    use avro format not JSON

--- producers/models/station.py
+++ producers/models/station.py
@@ -43,6 +43,7 @@ class Station(Producer):
         
         self.turnstile = Turnstile(self)
 
+
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
         logger.info("arrival kafka integration complete")
